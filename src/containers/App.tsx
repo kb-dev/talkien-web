58776ffa4307dd239c2e09b344c244505c379202
Ludivine Capelli <ludivinecapelli@MacBook-Pro-de-Ludivine.local>
2019-09-03 12:18:50 +0200

    feat(azpp): Add search page (WIP)

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -8,17 +8,13 @@ const App: React.FC = () => {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
+				<div className="Rectangle">
+					<p className="TALKIEN">TALKIEN</p>
+					<p className="Recherche">Recherche</p>
+					<p className="Gestion">Gestion</p>
+					<p className="GitHub">GitHub</p>
+				</div>
+				
 			</header>
 		</div>
 	);
